dc2abe4dab96f42dbd4b3293ecd9495d449a2482
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2015-11-10 22:13:59 -0200

    Fix bug when coin is at 2nd position and is lighter
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- rrlm13.pl
+++ rrlm13.pl
@@ -17,122 +17,122 @@ balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
   X1 + X2 + X3 + Y1 =:= X4 + Z1 + Z2 + Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   X1 < X3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   X1 > X3.
 
-balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
-  X1 < X3.
+  X1 =:= X3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   X4 =:= Z1.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], diferente) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   X4 =\= Z1.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   X4 =:= Z1.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], diferente) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   X4 =\= Z1.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   X1 < X3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   X1 > X3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   X1 =:= X3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   Z1 < Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   Z1 > Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor),
   Z1 =:= Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   Z1 < Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   Z1 > Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior),
   Z1 =:= Z3.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 < Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 > Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], menor),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], menor),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 =:= Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 < Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 > Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], maior),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], maior),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Y2 =:= Y4.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], menor) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Z4 < X1.
 
 balanca3([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], maior) :-
-  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4 | T], igual),
+  balanca1([X1, X2, X3, X4, Y1, Y2, Y3, Y4], igual),
   balanca2([X1, X2, X3, X4, Y1, Y2, Y3, Y4, Z1, Z2, Z3, Z4], igual),
   Z4 > X1.
 
